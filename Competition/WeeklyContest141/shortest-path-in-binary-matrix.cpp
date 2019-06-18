6a72187c9899f85ff79412d7f6bd5d6e2aeec928
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-06-18 16:16:22 +0530

    Update shortest-path-in-binary-matrix.cpp

--- Competition/WeeklyContest141/shortest-path-in-binary-matrix.cpp
+++ Competition/WeeklyContest141/shortest-path-in-binary-matrix.cpp
@@ -1,7 +1,9 @@
-//Error
+
 //https://leetcode.com/problems/shortest-path-in-binary-matrix/
 
 
+/*
+//Minor Error
 class Solution {
 public:
     
@@ -33,5 +35,41 @@ public:
         if(memoize[grid.size()-1][grid[0].size()-1]==INT_MAX)
             return -1;
         return memoize[grid.size()-1][grid[0].size()-1];
+    }
+};
+*/
+
+
+
+
+
+class Solution {
+public:
+    struct path{
+        int i;
+        int j;
+        int len;
+    };    
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {        
+        vector<pair<int,int>> directions={{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        int n=grid.size();
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        queue<path> q;
+        q.push({0,0,1});
+        grid[0][0]=1;
+        
+        while(!q.empty()){
+            path p=q.front();
+            q.pop();
+            for(auto x: directions){
+                int i=p.i+x.first,j=p.j+x.second;
+                if(i==n-1 && j==n-1) return p.len+1;
+                if(i<0 || i>=n || j<0 || j>=n || grid[i][j]==1) continue;
+                grid[i][j]=1;
+                q.push({i,j,p.len+1});
+            }
+        }
+        return -1;
     }
 };
