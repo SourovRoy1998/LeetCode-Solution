eec9f24c1475afa41f2bc07d0aa233043889353c
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-07-28 10:24:35 +0530

    Create n-th-tribonacci-number.cpp

---
+++ Competition/WeeklyContest147/n-th-tribonacci-number.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
+        int a=0,b=1,c=1,temp;
+        
+        for(int i=3;i<=n;i++){
+            temp=a+b+c;
+            a=b;
+            b=c;
+            c=temp;
+        }
+        
+       return temp; 
+            
+    }
+};
