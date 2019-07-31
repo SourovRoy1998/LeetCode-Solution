5e1c765f303aa0f0886f8eed902659c00cb7ec17
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-07-31 15:52:18 +0530

    Update next-greater-element-iii.cpp

--- Practice/next-greater-element-iii.cpp
+++ Practice/next-greater-element-iii.cpp
@@ -10,3 +10,27 @@ public:
         return (num>INT_MAX || num<=n) ? -1:num;
     }
 };
+
+
+//My Implementation
+class Solution {
+public:
+    void next_permutation(string& s){
+        int n=s.length();
+        int i=n-2;
+        while(i>=0 && s[i]>=s[i+1]) i--;
+        if(i==-1) return;
+        int j=i;
+        while(j<n-1 && s[j+1]>s[i]) j++;
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1, s.end());
+    }
+    
+    
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        next_permutation(s);
+        long long num=stoll(s);
+        return (num>INT_MAX || num<=n) ? -1:num;
+    }
+};
