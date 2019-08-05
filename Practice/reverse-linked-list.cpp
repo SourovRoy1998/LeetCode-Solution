c41e98731ceb30b26ef6be8dc971ec528f1367d2
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-05 17:32:28 +0530

    Update reverse-linked-list.cpp

--- Practice/reverse-linked-list.cpp
+++ Practice/reverse-linked-list.cpp
@@ -1,5 +1,8 @@
 //https://leetcode.com/problems/reverse-linked-list/
 
+
+
+//Iterative Approach 
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -21,5 +24,36 @@ public:
         }
         
         return prev;
+    }
+};
+
+
+
+
+//Recursive Approach
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* new_head;
+    void reverse(ListNode* head){
+        if(!head->next){
+            new_head=head;
+            return;
+        }
+        reverse(head->next);
+        head->next->next=head;
+        head->next=NULL;
+    }
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        reverse(head);
+        return new_head;
     }
 };
