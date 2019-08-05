4fc73a819128668ce6b10f4367947ea644eca00e
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-05 17:36:21 +0530

    Update reverse-linked-list.cpp

--- Practice/reverse-linked-list.cpp
+++ Practice/reverse-linked-list.cpp
@@ -57,3 +57,27 @@ public:
         return new_head;
     }
 };
+
+
+
+
+//Simpler Recursive Approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* new_head=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return new_head;
+    }
+};
