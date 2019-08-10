6a10fd21b81e2a45b535147ec1c62765a3470d7c
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-10 21:50:10 +0530

    Create analyse-user-website-visit-pattern.py

---
+++ Competition/BiWeeklyContest6/analyse-user-website-visit-pattern.py
@@ -0,0 +1,13 @@
+##https://leetcode.com/problems/analyse-user-website-visit-pattern/
+
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        import collections
+        dp = collections.defaultdict(list)
+        count = collections.Counter()
+        for t, u, w in sorted(zip(timestamp, username, website)):
+            dp[u].append(w)
+        for u in dp:
+            count += collections.Counter(set(seq for seq in itertools.combinations(dp[u], 3)))
+        target = max(count.values())
+        return min(list(k) for k in count if count[k] == target)
