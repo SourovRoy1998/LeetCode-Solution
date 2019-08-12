d1d7841b20380c459af5c285c3386f7611029d10
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-12 22:22:21 +0530

    Create online-majority-element-in-subarray.cpp

---
+++ Competition/WeeklyContest149/online-majority-element-in-subarray.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/online-majority-element-in-subarray/
+
+
+
+class MajorityChecker {
+public:
+    unordered_map<int,vector<int>> hmap;
+    vector<int> arr;
+
+    MajorityChecker(vector<int>& A) {
+        for(int i=0;i<A.size();i++){
+            hmap[A[i]].push_back(i);
+            arr.push_back(A[i]);
+        }
+    }
+
+    int query(int left, int right, int threshold) {
+        for(int i=0;i<10;i++){
+            int x=arr[left+rand()%(max(right-left,1))];
+            int k=upper_bound(hmap[x].begin(), hmap[x].end(),right)-lower_bound(hmap[x].begin(), hmap[x].end(),left);
+            if(k>=threshold)
+                return x;
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your MajorityChecker object will be instantiated and called as such:
+ * MajorityChecker* obj = new MajorityChecker(arr);
+ * int param_1 = obj->query(left,right,threshold);
+ */
