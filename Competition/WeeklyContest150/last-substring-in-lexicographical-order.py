2ddcfbe9ef77cd8d316adc43cfac5d847042ca1d
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-21 19:56:31 +0530

    Update last-substring-in-lexicographical-order.py

--- Competition/WeeklyContest150/last-substring-in-lexicographical-order.py
+++ Competition/WeeklyContest150/last-substring-in-lexicographical-order.py
@@ -8,5 +8,47 @@ class Solution:
         for i in range(len(s)):
             result=max(result,s[i:])
         return result
-
+    
+    
+    
+  '''
+class Solution:
+    def lastSubstring(self, s: str) -> str:
+        count=collections.defaultdict(list)
+        for i in range(len(s)):
+            count[s[i]].append(i)
+        largeC=max(count.keys())
+        starts={}
+        for pos in count[largeC]:
+            starts[pos]=pos+1
+		# Initialize all candidates and their pointers
+		
+        while len(starts)>1:
+		# Eliminate till we have only one
+            toDel=set()
+            nextC=collections.defaultdict(list)
+            for start,end in starts.items():
+                if end==len(s):
+				# Remove if reaching the end
+                    toDel.add(start)
+                    continue
+					
+                nextC[s[end]].append(start)
+				# Filter by current letter
+				
+                if end in starts:
+                    toDel.add(end)
+				# "Swallow" the latter candidate
+			
+            nextStarts={}
+            largeC=max(nextC.keys())
+            for start in nextC[largeC]:
+                if start not in toDel:
+                    nextStarts[start]=starts[start]+1
+					# Select what we keep for the next step
+            starts=nextStarts.copy()
+        for start,end in starts.items():
+            return s[start:]
+            
+        '''
 
