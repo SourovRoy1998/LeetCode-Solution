39f0f61b22aa61f33b3c35f6eaadc5a552df5917
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-30 17:38:53 +0530

    Update reverse-string-ii.cpp

--- Practice/reverse-string-ii.cpp
+++ Practice/reverse-string-ii.cpp
@@ -16,3 +16,23 @@ public:
         return answer;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i=0;i<s.length();i+=2*k)
+            reverse(s.begin()+i,min(s.end(),s.begin()+i+k));
+        return s;
+    }
+};
