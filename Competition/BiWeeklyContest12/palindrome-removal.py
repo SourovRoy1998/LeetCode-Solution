5a3d7434b2bef0f2686e926be338f8cc0aa1ad39
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-11-02 22:11:16 +0530

    Update palindrome-removal.py

--- Competition/BiWeeklyContest12/palindrome-removal.py
+++ Competition/BiWeeklyContest12/palindrome-removal.py
@@ -1,5 +1,6 @@
 ##https://leetcode.com/problems/palindrome-removal/
 
+'''
 class Solution:
     def minimumMoves(self, A):
         @lru_cache(None)
@@ -7,4 +8,20 @@ class Solution:
             if i > j: return 0
             return min(dp(i, k - 1) + (dp(k + 1, j - 1) if k < j - 1 else 1) for k in range(i, j + 1) if A[k] == A[j])
         return dp(0, len(A) - 1)
+'''      
+
         
+from functools import lru_cache
+class Solution(object):
+    def minimumMoves(self, A):
+        @lru_cache(None)
+        def dp(i, j):
+            if i > j: return 0
+            res = dp(i, j - 1) + 1
+            if A[j] == A[j - 1]:
+                res = min(res, dp(i, j - 2) + 1)
+            for k in range(i, j - 1):
+                if A[j] == A[k]:
+                    res = min(res, dp(i, k - 1) + dp(k + 1, j - 1) if k < j - 1 else 1)
+            return res
+        return dp(0, len(A) - 1)
