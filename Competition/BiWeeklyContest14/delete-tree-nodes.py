07d101f504313aa689c3e72ba69aaa1cccde0c02
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-12-01 10:27:30 +0530

    Update delete-tree-nodes.py

--- Competition/BiWeeklyContest14/delete-tree-nodes.py
+++ Competition/BiWeeklyContest14/delete-tree-nodes.py
@@ -14,3 +14,19 @@ class Solution:
                 count += c if t else 0
             return total, count if total else 0
         return dfs(0)[1]
+    
+    
+    
+    '''
+    class Solution {
+public:
+    int deleteTreeNodes(int nodes, vector<int>& parent, vector<int>& value) {
+        vector<int> result(nodes);
+        for(int i=nodes-1;i>0;i--){
+            value[parent[i]]+=value[i];
+            result[parent[i]]+=value[i]?result[i]+1:0;
+        }
+        return value[0]?result[0]+1:0;
+    }
+};
+'''
