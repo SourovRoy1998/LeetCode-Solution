c7f63c4ad528e123c0828c3681a3ddb9d3a40137
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-12-28 21:45:40 +0530

    Create number-of-paths-with-max-score.py

---
+++ Competition/BiWeeklyContest16/number-of-paths-with-max-score.py
@@ -0,0 +1,16 @@
+##https://leetcode.com/contest/biweekly-contest-16/problems/number-of-paths-with-max-score/
+
+ def pathsWithMaxScore(self, A):
+        n, mod = len(A), 10**9 + 7
+        dp = [[[-10**5, 0] for j in xrange(n + 1)] for i in xrange(n + 1)]
+        dp[n - 1][n - 1] = [0, 1]
+        for x in range(n)[::-1]:
+            for y in range(n)[::-1]:
+                if A[x][y] in 'XS': continue
+                for i, j in [[0, 1], [1, 0], [1, 1]]:
+                    if dp[x][y][0] < dp[x + i][y + j][0]:
+                        dp[x][y] = [dp[x + i][y + j][0], 0]
+                    if dp[x][y][0] == dp[x + i][y + j][0]:
+                        dp[x][y][1] += dp[x + i][y + j][1]
+                dp[x][y][0] += int(A[x][y]) if x or y else 0
+        return [dp[0][0][0] if dp[0][0][1] else 0, dp[0][0][1] % mod]
